5dc01d3f85a9a0a3944d099d223faf25cf21616b
Andrei Trasca <andrei.trasca1702>
2020-04-05 23:25:49 +0300

    second commit

--- loader/loader.c
+++ loader/loader.c
@@ -52,7 +52,7 @@ static void sig_handler(int signum, siginfo_t *sig, void *context)
         uintptr_t start = exec->segments[i].vaddr;
         uintptr_t end = exec->segments[i].vaddr + exec->segments[i].mem_size;
         if (start <= (uintptr_t)(sig->si_addr) && end >= (uintptr_t)(sig->si_addr)) {
-            if ((data_t)(exec->segments[i]).data->added == 1) {
+            if ((data_t *)(exec->segments[i].data)->added == 1) {
                 i = exec->segments_no;
                 break;
             }
@@ -62,7 +62,7 @@ static void sig_handler(int signum, siginfo_t *sig, void *context)
             start_addr = (char *)start;
             offset = exec->segments[i].offset;
             size = exec->segments[i].file_size;
-            (data_t)(exec->segments[i]).data->added = 1;
+            (data_t *)(exec->segments[i].data)->added = 1;
             break;
         }
     }
