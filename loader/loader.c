e31d1b8c93ef9fd53af035f74b0094157cf062c9
Andrei Trasca <andrei.trasca1702>
2020-04-05 22:35:46 +0300

    second commit

--- loader/loader.c
+++ loader/loader.c
@@ -36,7 +36,7 @@ static void sig_handler(int signum, siginfo_t *sig, void *context)
     for (i = 0; i < exec->segments_no; i++) {
         uintptr_t start = exec->segments[i].vaddr;
         uintptr_t end = exec->segments[i].vaddr + exec->segments[i].mem_size;
-        if (start <= sig->si_addr && end >= sig->si_addr) {
+        if (start <= (int *)sig->si_addr && end >= (int *)sig->si_addr) {
             pageno = ((char *)sig->si_addr - start) / getpagesize();
             perm = exec->segments[i].perm;
             start_addr = (char *)start;
